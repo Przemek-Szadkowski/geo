53494994b2f5b921c4b551b2981db4920bc715e6
Przemek-Szadkowski <przemoszadkowski@o2.pl>
2020-03-27 20:04:12 +0100

    Add function setLocation()

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const lati = document.querySelector('.lati');
 const long = document.querySelector('.long');
 const button = document.querySelector('button');
+const point = document.querySelector('.point');
 
 function showPosition(position) {
     const lattitude = position.coords.latitude;
     const longtitude = position.coords.longitude;
     lati.innerHTML = `X: ${lattitude}`;
     long.innerHTML = `Y: ${longtitude}`;
+    setLocation(lattitude, 50);
 }
 
 function errorHandling(err) {
@@ -20,5 +22,13 @@ function getLocation() {
         communicate.innerHTML = `Twoje urządzenie nie wspiera geolokalizacji`;
     }
 }
+
+function setLocation(lati, long) {
+    const top = (lati / 100) * 51.5027447;
+    document.documentElement.style.setProperty('--top', `${top}%`);
+    document.documentElement.style.setProperty('--left', `${long}%`);
+}
+
+// setLocation(25, 10);
 
 button.addEventListener('click', getLocation);
