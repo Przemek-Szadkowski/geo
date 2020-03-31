3ab68bb95bf92c9132f2e798d867bc04ea4dc611
Przemek-Szadkowski <przemoszadkowski@o2.pl>
2020-03-31 17:03:10 +0200

    Update top and left condition

--- app.js
+++ app.js
@@ -2,13 +2,21 @@ const lati = document.querySelector('.lati');
 const long = document.querySelector('.long');
 const button = document.querySelector('button');
 const point = document.querySelector('.point');
+const coordinates = document.querySelector('.coordinates');
+
+let isOnTheMap = true;
 
 function showPosition(position) {
     let lattitude = position.coords.latitude;
     const longtitude = position.coords.longitude;
-    lati.innerHTML = `X: ${lattitude}`;
-    long.innerHTML = `Y: ${longtitude}`;
     setLocation(lattitude, longtitude);
+    if(isOnTheMap) {
+        lati.innerHTML = `X: ${lattitude}`;
+        long.innerHTML = `Y: ${longtitude}`;
+    } else {
+        coordinates.innerHTML = '<h2>Znajdujesz się poza obszarem mapy!</h2>';
+        point.style.display = "none";
+    }
 }
 
 function errorHandling(err) {
@@ -18,17 +26,20 @@ function errorHandling(err) {
 function getLocation() {
     if(navigator.geolocation) {
        navigator.geolocation.getCurrentPosition(showPosition, errorHandling, { enableHighAccuracy: true });
-       console.log(id);
     } else {
         communicate.innerHTML = `Twoje urządzenie nie wspiera geolokalizacji`;
     }
 }
 
 function setLocation(lati, long) {
-    // const top = 51.503503 - ((51.503503 - lati) * (100 / (51.503503 - 51.502744)));
-    const top = 100 - ((lati - 51.502744) * 100) / (51.503503 - 51.502744);
-    // const left = 19.1634806 - ((19.1634806 - long) * (100 / (19.1634806 - 19.1629098)));
-    const left = 100 -((long - 19.1629098) * 100) / (19.1634806 - 19.1629098);
+    let top = 100 - ((lati - 51.502744) * 100) / (51.503503 - 51.502744);
+    let left = 100 - ((long - 19.1629098) * 100) / (19.1634806 - 19.1629098);
+    console.log(top, left);
+    if((top < 0 || top > 100) || (left < 0 || left > 100)) {
+        isOnTheMap = false;
+        top = -1;
+        left = -1;
+    }
     document.documentElement.style.setProperty('--top', `${top}%`);
     document.documentElement.style.setProperty('--left', `${left}%`);
 }
